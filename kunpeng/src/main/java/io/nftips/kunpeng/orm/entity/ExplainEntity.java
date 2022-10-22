727973e1d5aa77609ee762156356e0704e8859ff
zhoujunwen <zhoujunwen@proya.com>
2022-10-22 23:12:16 +0800

    增加接口文档

---
+++ kunpeng/src/main/java/io/nftips/kunpeng/orm/entity/ExplainEntity.java
@@ -0,0 +1,16 @@
+package io.nftips.kunpeng.orm.entity;
+
+import lombok.Data;
+
+/**
+ * @author zhoujunwen
+ * @date 2022-10-2022/10/22 下午10:31
+ **/
+@Data
+public class ExplainEntity {
+    private String id;
+    private String estRows;
+    private String task;
+    private String accessObject;
+    private String operatorInfo;
+}
